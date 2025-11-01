834865e4c270edb42a2c1732e6f2c151cc186750
AnkitSeal2023 <ankitseal2021@gmail.com>
2025-11-01 23:47:32 +0530

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -142,7 +142,7 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 vim.keymap.set('n', '<A-k>', '<cmd>m .-2<CR>==')
 vim.keymap.set('n', '<A-j>', '<cmd>m .+1<CR>==')
-vim.keymap.set('n', 'v', '^v$', {})
+vim.keymap.set('n', 'V', '^v$', {})
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -431,7 +431,12 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.getcwd(),
+        }
+      end, { desc = '[S]earch [F]iles' })
+
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
